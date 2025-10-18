b9052f9af8ebebce35ace57db9026182ff713f6a
Simon Cossar <scossar3@gmail.com>
2025-10-18 13:52:35 -0700

    updates

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,13 @@
-return {
-  "catppuccin/nvim",
-  opts = function(_, opts)
-    local module = require("catppuccin.groups.integrations.bufferline")
-    if module then
-      module.get = module.get_theme
-    end
-    return opts
-  end,
-}
+if true then
+  return {}
+end
+-- return {
+--   "catppuccin/nvim",
+--   opts = function(_, opts)
+--     local module = require("catppuccin.groups.integrations.bufferline")
+--     if module then
+--       module.get = module.get_theme
+--     end
+--     return opts
+--   end,
+-- }
